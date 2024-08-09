047afcf5f0fa452d6cc1f607a47fd01e858ee7b0
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-08-09 10:43:18 +0300

    Job List

---
+++ src/components/JobList.js
@@ -0,0 +1,134 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { Tabs, Tab, Typography, Box } from "@material-ui/core";
+import FadeInSection from "./FadeInSecton";
+
+const isHorizontal = window.innerWidth < 600;
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  if (isHorizontal) {
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  if (isHorizontal) {
+    return {
+      id: `full-width-tab-${index}`,
+      "aria-controls": `full-width-tabpanel-${index}`
+    };
+  } else {
+    return {
+      id: `vertical-tab-${index}`
+    };
+  }
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    height: 300
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`
+  }
+}));
+
+const JobList = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const experienceItems = {
+    TechnoBrain: {
+      jobTitle: "Software Developer Intern @",
+      duration: "APRIL 2024 - SEPTEMBER 2024",
+      desc: [
+        "Optimized database queries to improve application performance and reduce response times",
+        "Refactored legacy code to improve reliability, scalability and maintainability",
+        "Developed full stack web application with a modern user interface that improved user engagement",
+        "Optimized database performance through query optimization and indexing",
+        "Collaborated with team members to design, develop, and launched user-friendly software products",
+        "Worked on Odoo customizations, understood the existing codes and made several customizations which were then deployed to clients"
+      ]
+    }
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation={!isHorizontal ? "vertical" : null}
+        variant={isHorizontal ? "fullWidth" : "scrollable"}
+        value={value}
+        onChange={handleChange}
+        className={classes.tabs}
+      >
+        {Object.keys(experienceItems).map((key, i) => (
+          <Tab key={i} label={isHorizontal ? `0${i}.` : key} {...a11yProps(i)} />
+        ))}
+      </Tabs>
+      {Object.keys(experienceItems).map((key, i) => (
+        <TabPanel key={i} value={value} index={i}>
+          <span className="joblist-job-title">
+            {experienceItems[key]["jobTitle"] + " "}
+          </span>
+          <span className="joblist-job-company">{key}</span>
+          <div className="joblist-duration">
+            {experienceItems[key]["duration"]}
+          </div>
+          <ul className="job-description">
+            {experienceItems[key]["desc"].map((descItem, j) => (
+              <FadeInSection key={j} delay={`${j + 1}00ms`}>
+                <li>{descItem}</li>
+              </FadeInSection>
+            ))}
+          </ul>
+        </TabPanel>
+      ))}
+    </div>
+  );
+};
+
+export default JobList;
