e1e64c5dc2bf010d41edef6a2f8df197d30a2f1c
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-08-09 10:40:17 +0300

    Intro

---
+++ src/components/Intro.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSecton";
+import FractalTree from "./FractalTree";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <FractalTree></FractalTree>
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"Hi, "}
+            <span className="intro-name">{"Mark Kimathi"}</span>
+            {" here."}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+          Dedicated Computer Science student and proficient software developer with a
+          strong passion for designing, installing, testing, and maintaining sophisticated
+          software systems. Creativity and technical expertise span across various platforms
+          and programming languages, enabling to craft innovative solutions tailored to
+          complex problems. Excel in both independent projects and collaborative team
+          environments.
+          </div>
+          <a
+            href="mailto:markangelkimathi@gmail.com"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {" Say hi!"}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
